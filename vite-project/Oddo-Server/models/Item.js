58cd42e645becb41368c61a8068be3be1547f356
Iamkaran00 <161184262+Iamkaran00@users.noreply.github.com>
2025-07-12 11:01:41 +0530

    Update Item.js

---
+++ vite-project/Oddo-Server/models/Item.js
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+
+    Owner : {type : mongoose.Schema.Types.ObjectId,ref : "User"},
+    title : String,
+    description : String,
+    category : String,
+    type : String,//shirt , pants
+    size : String, //l , m , xl , xxl
+    condition : {type : String,enum :["New",'Like New','Good',"fair"]},
+    tags : [String],
+    isApproved : {type : Boolean,default : false},
+    isAvailable : {type : Boolean,default : true},
+    swapRequestedBy : {type : mongoose.Schema.Types.ObjectId,ref : "User",default :null},
+    redeemedBy : {type : mongoose.Schema.Types.ObjectId,ref:"User",default : null}
+
+},
+{timestamps:true});
+const Item = mongoose.model('Item',itemSchema);
+export default Item;
