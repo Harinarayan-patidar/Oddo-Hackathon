fccdc5550c5889358f0dabbefc6f03ba59eec2d8
kavendra2004 <kavendraahirwar2004@gmail.com>
2025-07-12 10:59:58 +0530

    User model updated

--- vite-project/Oddo-Server/models/User.js
+++ vite-project/Oddo-Server/models/User.js
@@ -1,61 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstName :{
-        type:String,
-        required: true, 
-        trim:true,
-    },
-    lastName:{
-        type:String,
-        required:true,
-        trim:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    accountType:{
-        type:String,
-        enum:["Admin", "Student", "Instructor"],
-        required:true,
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  points: { type: Number, default: 0 },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+});
 
-    },
-    additionalDetails:{
-        type:mongoose.Schema.Types.ObjectId,
-    
-        ref:"Profile",
-    },
-    courses:[
-        {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Course"
-        }
-    ],
-    image:{
-        type:String,
-        required:true,
-
-    },
-    courseProgress:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"CourseProgress",
-        }
-    ],
-    token:{
-        type:String,
-
-    },
-    resetPasswordExpires:{
-        type:Date,
-    },
-
-})
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
